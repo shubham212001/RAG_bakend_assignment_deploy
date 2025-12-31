2a7e544ff7ca037486b0630b6c58940b225a4de1
shubham212001 <shubham.sharma200121@gmail.com>
2025-12-31 23:46:02 +0530

    21

--- backend/services/uploadService.js
+++ backend/services/uploadService.js
@@ -65,7 +65,7 @@ const { v4: uuidv4 } = require('uuid');
 
 // async function splitTextAndStore(text, filePath, chunkSize) {
 //   const client = await connectToWeaviate();
-//   const documentCollection = client.collections.get('cluster');
+//   const documentCollection = client.collections.get('test-cluster');
 
 //   await checkAndDeleteExistingData(documentCollection, filePath);
 
@@ -105,7 +105,7 @@ const { v4: uuidv4 } = require('uuid');
 //     const embedding = await embeddingService.generateEmbedding(chunkText);
 //     return {
 //       id: uuidv4(),
-//       class: "cluster",
+//       class: "test-cluster",
 //       properties: {
 //         page_content: chunkText,
 //         file_name: filePath,
@@ -129,7 +129,7 @@ const { v4: uuidv4 } = require('uuid');
 
 async function splitTextAndStore(text, filePath, chunkSize) {
   const client = await connectToWeaviate();
-  const documentCollection = client.collections.get('cluster');
+  const documentCollection = client.collections.get('test-cluster');
 
   await checkAndDeleteExistingData(documentCollection, filePath);
 
@@ -146,7 +146,7 @@ async function splitTextAndStore(text, filePath, chunkSize) {
     const embedding = await embeddingService.generateEmbedding(chunkText);
     return {
       id: uuidv4(),
-      class: "cluster",
+      class: "test-cluster",
       properties: {
         page_content: chunkText,
         file_name: filePath,
